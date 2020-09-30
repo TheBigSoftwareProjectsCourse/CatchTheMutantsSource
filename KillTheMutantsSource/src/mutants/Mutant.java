27df0608373e2d21ff99d11f267e7a549ab21713
Sara Sprenkle <sprenkles@wlu.edu>
2020-09-30 15:23:28 -0400

    Updated Javadocs for Mutant interface and Wolverine

--- KillTheMutantsSource/src/mutants/Mutant.java
+++ KillTheMutantsSource/src/mutants/Mutant.java
@@ -18,13 +18,14 @@ public interface Mutant {
 	 * The original array should not be altered.
 	 *  
 	 * Examples:
-	 *    thirdShortest(["a", "ab", "and"]) returns "a"
+	 *    thirdShortest(["a", "ab", "abc"]) returns "abc"
 	 *    thirdShortest(["a", "b", "bc", "ab", "bye", "and"]) returns "bye" or "and" because
 	 *        “a” and “b” are the shortest, “ab” and “bc” are the second shortest, and “bye” and “and” are the third shortest.
 	 *    thirdShortest(["a"]) should throw an IllegalArgumentException
 	 * 
 	 * @param words an array of Strings
 	 * @return a String giving the third shortest String from the array
+	 * @throws IllegalArgumentException if array is null or if there is no third shortest
 	 */
 	public String thirdShortest(String[] words);
 }
--- KillTheMutantsSource/src/mutants/Wolverine.java
+++ KillTheMutantsSource/src/mutants/Wolverine.java
@@ -5,53 +5,67 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public class Wolverine implements Mutant{
+/**
+ * Correct implementation of the method thirdShortest
+ * 
+ * @author Shannon Duvall original concept
+ * @author Sara Sprenkle documentation
+ *
+ */
+public class Wolverine implements Mutant {
 	
-	// -------- PROBLEM: Third shortest
-	//  input: strands, an array of Strings
-	// output: a String giving the third shortest String from the array
-	// method: return the String in the array that is the third shortest. 
-	//			If there IS NO THIRD SHORTEST in the array or if the array is null the code should 
-	//			throw an IllegalArgumentException.
-	//			If there is a tie for the third shortest, any of the tied strings is valid.
-	//			If there is a tie for shortest or second shortest, the duplicates do not 
-	//						affect the calculation of the third shortest.
-	//			The original array should not be altered.
-	// Examples:	
-	//		thirdShortest(["a", "ab", "and"]) = "and"
-	//		thirdShortest(["a", "b", "bc", "ab", "bye", "and"]) = "bye" or "and"
-	//		thirdShortest(["a"]) should throw an IllegalArgumentException
-
-
-		public String thirdShortest(String[] words){
-			if(words == null) throw new IllegalArgumentException();
-			if(words.length<3) throw new IllegalArgumentException("Array input too short");
-			List<String> list = Arrays.asList(words);
-			//System.out.println(list);
-			list.sort(Comparator.comparingInt(String::length));
-			//System.out.println(list);
-			List<String> noDuplicates = new ArrayList<String>(list);
-			String last = null;
-			for(String word: list){
-				if(last == null) last = word;
-				else if(word.length()==last.length()){
-						noDuplicates.remove(word);
-				}
-				else{
-					last = word;
-				}
+	/**
+	 * Returns the third shortest String in the array
+	 * 
+	 * If there IS NO THIRD SHORTEST in the array or if the array is null, the code
+	 * should throw an IllegalArgumentException.
+	 * If there is a tie for the third shortest, any of the tied strings is valid.
+	 * If there is a tie for shortest or second shortest, the duplicates do not
+	 * affect the calculation of the third shortest. 
+	 * The original array should not be altered.
+	 *  
+	 * Examples:
+	 *    thirdShortest(["a", "ab", "abc"]) returns "abc"
+	 *    thirdShortest(["a", "b", "bc", "ab", "bye", "and"]) returns "bye" or "and" because
+	 *        “a” and “b” are the shortest, “ab” and “bc” are the second shortest, and “bye” and “and” are the third shortest.
+	 *    thirdShortest(["a"]) should throw an IllegalArgumentException
+	 * 
+	 * @param words an array of Strings
+	 * @return a String giving the third shortest String from the array
+	 * @throws IllegalArgumentException if array is null or if there is no third shortest
+	 */
+	public String thirdShortest(String[] words) {
+		if (words == null)
+			throw new IllegalArgumentException();
+		if (words.length < 3)
+			throw new IllegalArgumentException("Array input too short");
+		List<String> list = Arrays.asList(words);
+		// System.out.println(list);
+		list.sort(Comparator.comparingInt(String::length));
+		// System.out.println(list);
+		List<String> noDuplicates = new ArrayList<String>(list);
+		String last = null;
+		for (String word : list) {
+			if (last == null)
+				last = word;
+			else if (word.length() == last.length()) {
+				noDuplicates.remove(word);
+			} else {
+				last = word;
 			}
-			if(noDuplicates.size()<3) throw new IllegalArgumentException("Array does not have third shortest.");
-			String ideal = noDuplicates.get(2);
-			List<String> answers = new ArrayList<String>();
-			for(String s: list){
-				if(s.length()==ideal.length()){
-					answers.add(s);
-				}
-			}
-			int index = (int)(Math.random()*answers.size());
-			return answers.get(index);	
 		}
+		if (noDuplicates.size() < 3)
+			throw new IllegalArgumentException("Array does not have third shortest.");
+		String ideal = noDuplicates.get(2);
+		List<String> answers = new ArrayList<String>();
+		for (String s : list) {
+			if (s.length() == ideal.length()) {
+				answers.add(s);
+			}
+		}
+		int index = (int) (Math.random() * answers.size());
+		return answers.get(index);
+	}
 		
 
 		// -------- PROBLEM 2: Valid DNA
